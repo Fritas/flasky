8b97509802753fb943dda8ca724832c312351ff9
Aluno <adamascenosj@gmail.com>
2018-10-30 16:01:48 -0300

    Update: add config

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+Configurações da aplicação
+"""
+
+DEBUG = True
+#local do banco de dados
+SQLALCHEMY_DATABASE_URI = 'sqlite:////storage.db'
+SQLALCHEMY_TRACK_MODIFICATIONS =  True #evita erros
